755a166040f0d3ea14833849c75e6d5e5a0e5ae1
yen <japocoin@gmail.com>
2019-03-19 12:27:09 +0100

    complete exercise

---
+++ docs/precache-manifest.f12a244b29a0af7eb63a2e20a705922d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c331ceeb8995950d09fa",
+    "url": "./static/js/main.b49d5c8d.chunk.js"
+  },
+  {
+    "revision": "4e06cbc20a459c856eea",
+    "url": "./static/js/2.e0b6258c.chunk.js"
+  },
+  {
+    "revision": "c331ceeb8995950d09fa",
+    "url": "./static/css/main.35c60867.chunk.css"
+  },
+  {
+    "revision": "4f0c788930d05ddb672d93cf24deaa81",
+    "url": "./index.html"
+  }
+];
